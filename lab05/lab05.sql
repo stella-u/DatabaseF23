0b552885ac9381f0ac69faeaac27587801d303a2
Stella Ullman <stellarainullman@gmail.com>
2023-10-03 17:06:21 +0000

    debugging lab05

--- lab05/lab05.sql
+++ lab05/lab05.sql
@@ -86,34 +86,41 @@ VALUES
     ('silk'),
     ('wool'),
     ('cotton');
+
 INSERT INTO states(state_code, state_name)
 VALUES
     ('CA', 'California'),
     ('OH', 'Ohio'),
     ('GA', 'Georgia'),
     ('PA', 'Pennslyvania');
+
 INSERT INTO countries(country_name)
 VALUES
     ('India'),
     ('Afghanistan'),
     ('Iran'),
     ('Turkey');
+
 INSERT INTO customers(customer_first_name, customer_last_name, street_address, 
     city_address, state_code, zipcode_address, phone_number)
 VALUES
     ('Gloria', 'Gomez', '78 Corona Rd.', 'Fullerton', 'CA', '92831', NULL),
     ('Marco', 'Page', '747 East Harrison Lane', 'Atlanta', 'GA', '30303', '(588) 799-6535'),
     ('Sandra', 'Page', '747 East Harrison Lane', 'Atlanta', 'GA', '30303', '(997) 697-2666');
+
 INSERT INTO rugs(country_id, rug_style, rug_year_made, rug_size, material_id,
      rug_purchase_price, rug_date_acquired, rug_markup)
 VALUES
     (4, 'Ushak', '1925', '5x7', 2, 625.00, '4/6/17', 100);
+
 INSERT INTO purchases(customer_id, rug_id, sale_date, sale_price, return_date)
 VALUES
     (1, 1, '12/14/17', 990.00);
+
 INSERT INTO trials (customer_id, rug_id, trial_start_date, trial_expected_end_date, trial_end_date)
 VALUES
     (1, 1, '12/01/17', '12/18/17', '12/14/17');
+
 
 SELECT customer_id, rug_id, sale_date, sale_price
     FROM purchases
